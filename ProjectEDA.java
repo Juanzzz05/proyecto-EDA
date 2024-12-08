f42565965739347fa7f24be75f2a74951ad94e53
Juanzzz05 <169114123+Juanzzz05@users.noreply.github.com>
2024-12-08 10:19:49 -0500

    Add files via upload

---
+++ ProjectEDA.java
@@ -0,0 +1,17 @@
+
+package project.eda;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class ProjectEDA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
